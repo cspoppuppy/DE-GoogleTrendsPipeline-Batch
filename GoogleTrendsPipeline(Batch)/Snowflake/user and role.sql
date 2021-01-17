27d3c2aea68123b905699895ab02d150477620ac
cspoppuppy <cspoppuppy@hotmail.com>
2021-01-17 19:52:54 +0000

    Create user and role.sql

---
+++ GoogleTrendsPipeline(Batch)/Snowflake/user and role.sql
@@ -0,0 +1,34 @@
+//==============================================================================
+// Create read only role for snowpipe database
+//==============================================================================
+create role snowpipe_read_only
+comment = 'This role is limited to querying tables in staging and public';
+
+grant usage
+on database snowpipe
+to role snowpipe_read_only;
+
+grant usage
+on schema snowpipe.staging
+to role snowpipe_read_only;
+
+grant select
+on all tables in schema snowpipe.staging
+to role snowpipe_read_only;
+
+grant usage
+on schema snowpipe.public
+to role snowpipe_read_only;
+
+grant select
+on all tables in schema snowpipe.public
+to role snowpipe_read_only;
+
+grant usage
+on warehouse compute_wh
+to role snowpipe_read_only;
+
+//==============================================================================
+// Create test_user
+//==============================================================================
+create user test_user password='GoogleTrends.1' default_role = snowpipe_read_only must_change_password = false;
